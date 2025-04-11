77136e1219c6af83fd20930b1074e91b65e0d06f
nzaincreatives <work@nzain.me>
2025-04-11 12:05:54 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("chefsShareCache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('chefsShare-cache').then((cache) => {
+      return cache.addAll(['./index.html']);
     })
   );
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
